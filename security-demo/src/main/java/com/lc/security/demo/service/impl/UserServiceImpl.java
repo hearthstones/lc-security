4e1d98841b91da9c31e7796811b08d80753cb813
luchao <1967768885@qq.com>
2021-11-08 19:58:58 +0800

    接口定义.

---
+++ security-demo/src/main/java/com/lc/security/demo/service/impl/UserServiceImpl.java
@@ -0,0 +1,36 @@
+package com.lc.security.demo.service.impl;
+
+import com.lc.security.demo.pojo.dto.UserDTO;
+import com.lc.security.demo.pojo.vo.UserVO;
+import com.lc.security.demo.service.UserService;
+import org.springframework.stereotype.Service;
+
+/**
+ * UserServiceImpl
+ *
+ * @author luchao
+ * @date 2021/11/8
+ */
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Override
+    public void add(UserDTO dto) {
+
+    }
+
+    @Override
+    public void update(UserDTO dto) {
+
+    }
+
+    @Override
+    public void delete(Integer id) {
+
+    }
+
+    @Override
+    public UserVO detail(Integer id) {
+        return null;
+    }
+}
