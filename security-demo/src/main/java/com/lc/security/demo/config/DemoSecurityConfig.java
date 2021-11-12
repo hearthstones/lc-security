198f185b20c20c1aaf729065546bc1335beb4094
luchao <1967768885@qq.com>
2021-11-12 19:35:31 +0800

    认证、资源服务器.

--- security-demo/src/main/java/com/lc/security/demo/config/DemoSecurityConfig.java
+++ security-demo/src/main/java/com/lc/security/demo/config/DemoSecurityConfig.java
@@ -2,8 +2,10 @@ package com.lc.security.demo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -21,10 +23,22 @@ public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        return super.userDetailsService();
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
-//        http.httpBasic()
+//        http.formLogin()
+        http.httpBasic()
             .and()
             .authorizeRequests()
             .anyRequest()
---
+++ security-demo/src/main/java/com/lc/security/demo/server/AuthorizationServerConfig.java
@@ -0,0 +1,16 @@
+package com.lc.security.demo.server;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+/**
+ * 认证服务器
+ *
+ * @author luchao
+ * @date 2021/11/11
+ */
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig {
+
+}
